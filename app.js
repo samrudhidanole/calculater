c8b9a0d4ffa5b5a3fc4f65b78b948e7e494aa251
SamrudhiDhanole <110589466+samrudhidanole@users.noreply.github.com>
2023-09-02 12:21:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+//getnumber
+function number(num)
+{
+    var result = document.getElementById("input");
+    result.value += num;
+}
+
+//getresult
+
+function result()
+{
+    var result = document.getElementById("input");
+    result.value = eval(result.value)
+}
+
+//clear result
+
+function clearResult()
+{
+    var result = document.getElementById("input");
+    result.value = "";
+}
+//delete
+
+function deleteChar()
+{
+    var number = document.getElementById("input");
+    var remove = number.value;
+    remove = remove.slice(0,-1);//remove last character
+    number.value = remove;
+}
